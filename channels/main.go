fc024fa149f6cae229689c9407234cbbd02e5228
Jorge Morales <jorge.iv.morales@gmail.com>
2020-05-23 18:17:48 -0500

    Finished Channels chapter

---
+++ channels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(l string, c chan string) {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be done")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
